a78c2df8b2a4bc91be315b546fa9f48e88f7201d
ShadowKitty42 <shadowxdragonx42@gmail.com>
2026-02-15 17:12:52 -0800

    Added all necessary changes to get basic starting point

--- worlds/CotL/items.py
+++ worlds/CotL/items.py
@@ -1,3 +1,4 @@
+import typing
 from BaseClasses import Item, ItemClassification
 
 class CotLItem(Item):  # or from Items import MyGameItem
@@ -7,9 +8,11 @@ class CotLItem(Item):  # or from Items import MyGameItem
 class ItemData:
     itemName: str
     progression: ItemClassification
-    def __init__(self, itemName, progression):
+    upgrade_name: str
+    def __init__(self, itemName, progression, upgrade_name):
         self.itemName = itemName
         self.progression = progression
+        self.upgrade_name = upgrade_name
 
 
 mygame_items: typing.List[ItemData] = [
@@ -18,96 +21,96 @@ mygame_items: typing.List[ItemData] = [
 
     #Tier 1 
     #ItemData("Temple", ItemClassification.useful), #Type: TEMPLE
-    ItemData("Progressive Bed", ItemClassification.useful), #Type: BED
+    ItemData("Progressive Bed", ItemClassification.useful, "Prog_Bed"), #Type: BED
     #will need make this a progressive bed due to how its built sleeping bag
-    ItemData("Body Pit", ItemClassification.useful), #Type: BODY_PIT
+    ItemData("Body Pit", ItemClassification.useful, "Building_BodyPit"), #Type: BODY_PIT
     #may need logic due to possibility of killing all base from infection
-    ItemData("Farm Plot", ItemClassification.useful), #Type: FARM_PLOT
+    ItemData("Farm Plot", ItemClassification.useful, "Building_Farms"), #Type: FARM_PLOT
     #may need to be logic to keep cultists alive to open areas
-    ItemData("Progressive Farming Station", ItemClassification.useful), #Type: FARM_STATION
+    ItemData("Progressive Farming Station", ItemClassification.useful, "Prog_FarmStation"), #Type: FARM_STATION
     #may need to be logic to keep cultists alive to open areas
-    ItemData("Seed Silo", ItemClassification.useful), #Type: SILO_SEED
-    ItemData("Tailor", ItemClassification.filler), #Type: TAILOR
+    ItemData("Seed Silo", ItemClassification.useful, "Building_FollowerFarming"), #Type: SILO_SEED
+    ItemData("Tailor", ItemClassification.filler, "Building_Tailor"), #Type: TAILOR
     
     #Tier 2
-    ItemData("Progressive Shrine & Temple Upgrade", ItemClassification.useful), #Type: TEMPLE_II
+    ItemData("Progressive Shrine & Temple Upgrade", ItemClassification.useful, "Prog_Temple"), #Type: TEMPLE_II
     #Progressive
-    ItemData("Basic Decorations", ItemClassification.filler), 
-    ItemData("Progressive Bed", ItemClassification.useful), #Type: BED_2
+    ItemData("Basic Decorations", ItemClassification.filler, "Building_Decorations1"), 
+    ItemData("Progressive Bed", ItemClassification.useful, "Prog_Bed"), #Type: BED_2
     #Progressive bed 2 shelter
-    ItemData("Offering Statue", ItemClassification.useful), #Type: OFFERING_STATUE
-    ItemData("Progressive Tabernacle", ItemClassification.useful), #Type: SHRINE_PASSIVE
-    ItemData("Prison", ItemClassification.useful), #Type: PRISON
-    ItemData("Progressive Lumberyard", ItemClassification.progression), #Type: LUMBERJACK_STATION
-    ItemData("Progressive Stone Mine", ItemClassification.progression), #Type: BLOODSTONE_MINE
+    ItemData("Offering Statue", ItemClassification.useful, "Shrine_OfferingStatue"), #Type: OFFERING_STATUE
+    ItemData("Progressive Tabernacle", ItemClassification.useful, "Prog_Tabernacle"), #Type: SHRINE_PASSIVE
+    ItemData("Prison", ItemClassification.useful, "Building_Prison"), #Type: PRISON
+    ItemData("Progressive Lumberyard", ItemClassification.progression, "Prog_Lumberjack"), #Type: LUMBERJACK_STATION
+    ItemData("Progressive Stone Mine", ItemClassification.progression, "Prog_Mine"), #Type: BLOODSTONE_MINE
     #need to check note as I believe bloodstone mine is not what it said before I started the Woolhaven DLC
-    ItemData("Progressive Missionary", ItemClassification.useful), 
-    ItemData("Progressive Demonic Summoning Circle", ItemClassification.useful), #Type: DEMON_SUMMONER
+    ItemData("Progressive Missionary", ItemClassification.useful, "Prog_Missionary"), 
+    ItemData("Progressive Demonic Summoning Circle", ItemClassification.useful, "Prog_DemonSummoner"), #Type: DEMON_SUMMONER
     #should also be a progressive item
-    ItemData("Progressive Scarecrow", ItemClassification.useful), #Type: SCARECROW
+    ItemData("Progressive Scarecrow", ItemClassification.useful, "Prog_Scarecrow"), #Type: SCARECROW
     
     #Tier 3
-    ItemData("Progressive Refinery", ItemClassification.progression), #Type: REFINERY
+    ItemData("Progressive Refinery", ItemClassification.progression, "Prog_Refinery"), #Type: REFINERY
     #progressive
-    ItemData("Progressive Outhouse", ItemClassification.useful), #Type: OUTHOUSE
+    ItemData("Progressive Outhouse", ItemClassification.useful, "Prog_Outhouse"), #Type: OUTHOUSE
     #progressive
-    ItemData("Progressive Healing Bay", ItemClassification.useful), #Type: HEALING_BAY
-    ItemData("Progressive Janitor Station", ItemClassification.useful), #Type: JANITOR_STATION
+    ItemData("Progressive Healing Bay", ItemClassification.useful, "Prog_HealingBay"), #Type: HEALING_BAY
+    ItemData("Progressive Janitor Station", ItemClassification.useful, "Prog_Janitor"), #Type: JANITOR_STATION
     #progressive
-    ItemData("Empowered Shrine of Disciples", ItemClassification.useful), #Type: SHRINE_DISCIPLE_BOOST
-    ItemData("Cheaper Rituals", ItemClassification.useful), 
-    ItemData("Progressive Shrine Flame", ItemClassification.useful), 
-    ItemData("Confession Booth", ItemClassification.useful), #Type: CONFESSION_BOOTH
-    ItemData("Propoganda Speaker"), #Type: PROPAGANDA_SPEAKER
-    ItemData("Progressive Crypt", ItemClassification.useful), #
+    ItemData("Empowered Shrine of Disciples", ItemClassification.useful, "Building_Shrine_Disciple_Boost"), #Type: SHRINE_DISCIPLE_BOOST
+    ItemData("Cheaper Rituals", ItemClassification.useful, "Temple_CheaperRituals"), 
+    ItemData("Progressive Shrine Flame", ItemClassification.useful, "Prog_ShrineFlame"), 
+    ItemData("Confession Booth", ItemClassification.useful, "Building_ConfessionBooth"), #Type: CONFESSION_BOOTH
+    ItemData("Propaganda Speaker", ItemClassification.filler, "Building_PropagandaSpeakers"), #Type: PROPAGANDA_SPEAKER
+    ItemData("Progressive Crypt", ItemClassification.useful, "Prog_Crypt"), #
     #progressive
-    ItemData("Progressive Missionary", ItemClassification.useful), 
-    ItemData("Progressive Demonic Summoning Circle", ItemClassification.useful),
-    ItemData("Fertiliser Silo", ItemClassification.useful), 
-    ItemData("Progressive Harvest Totem", ItemClassification.useful), 
+    ItemData("Progressive Missionary", ItemClassification.useful, "Prog_Missionary"), 
+    ItemData("Progressive Demonic Summoning Circle", ItemClassification.useful, "Prog_DemonSummoner"),
+    ItemData("Fertiliser Silo", ItemClassification.useful, "Building_SiloFertiliser"), 
+    ItemData("Progressive Harvest Totem", ItemClassification.useful, "Prog_HarvestTotem"), 
 
     #Tier 4
-    ItemData("Progressive Shrine & Temple Upgrade", ItemClassification.useful), #Type: TEMPLE_III
+    ItemData("Progressive Shrine & Temple Upgrade", ItemClassification.useful, "Prog_Temple"), #Type: TEMPLE_III
     #progressive
-    ItemData("Shared Shelter", ItemClassification.useful), #Type: SHARED_HOUSE
-    ItemData("Progressive Bed", ItemClassification.useful), #Type: BED_3
+    ItemData("Shared Shelter", ItemClassification.useful, "Building_Shared_House"), #Type: SHARED_HOUSE
+    ItemData("Progressive Bed", ItemClassification.useful, "Prog_Bed"), #Type: BED_3
     #progressive bed 3 grand bed
-    ItemData("Progressive Janitor Station", ItemClassification.useful), #Type: JANITOR_STATION_2
+    ItemData("Progressive Janitor Station", ItemClassification.useful, "Prog_Janitor"), #Type: JANITOR_STATION_2
     #progressive
-    ItemData("Collected Shrine of Disciples", ItemClassification.useful), #Type: SHRINE_DISCIPLE_COLLECTION
-    ItemData("Ritual Cool Downs", ItemClassification.useful), 
-    ItemData("Progressive Tabernacle", ItemClassification.useful), 
-    ItemData("Progressive Shrine Flame", ItemClassification.useful), 
-    ItemData("Progressive Drink House", ItemClassification.useful), #Type: PUB
-    ItemData("Mating Tent", ItemClassification.useful), 
-    ItemData("Progressive Hatchery", ItemClassification.useful), 
-    ItemData("Progressive Crypt", ItemClassification.useful), 
-    ItemData("Progressive Morgue", ItemClassification.useful), 
-    ItemData("Progressive Refinery", ItemClassification.progression), 
-    ItemData("Bone Decorations", ItemClassification.filler), 
-    ItemData("Progressive Farming Station", ItemClassification.useful), 
-    ItemData("Compost", ItemClassification.useful), #Type: COMPOST_BIN
-    ItemData("Progressive Scarecrow", ItemClassification.useful), #Type: SCARECROW_2
+    ItemData("Collected Shrine of Disciples", ItemClassification.useful, "Building_Shrine_Disciple_Collection"), #Type: SHRINE_DISCIPLE_COLLECTION
+    ItemData("Ritual Cool Downs", ItemClassification.useful, "Temple_FasterCoolDowns"), 
+    ItemData("Progressive Tabernacle", ItemClassification.useful, "Prog_Tabernacle"), 
+    ItemData("Progressive Shrine Flame", ItemClassification.useful, "Prog_ShrineFlame"), 
+    ItemData("Progressive Drink House", ItemClassification.useful, "Prog_Pub"), #Type: PUB
+    ItemData("Mating Tent", ItemClassification.useful, "Building_MatingTent"),
+    ItemData("Progressive Hatchery", ItemClassification.useful, "Prog_Hatchery"),
+    ItemData("Progressive Crypt", ItemClassification.useful, "Prog_Crypt"), 
+    ItemData("Progressive Morgue", ItemClassification.useful, "Prog_Morgue"),
+    ItemData("Progressive Refinery", ItemClassification.progression, "Prog_Refinery"), 
+    ItemData("Bone Decorations", ItemClassification.filler, "Building_Decorations2"), 
+    ItemData("Progressive Farming Station", ItemClassification.useful, "Prog_FarmStation"), 
+    ItemData("Compost", ItemClassification.useful, "Followers_Compost"), #Type: COMPOST_BIN
+    ItemData("Progressive Scarecrow", ItemClassification.useful, "Prog_Scarecrow"), #Type: SCARECROW_2
     
     #Tier 5
-    ItemData("Progressive Shrine & Temple Upgrade", ItemClassification.useful), #Type: TEMPLE_IV
-    ItemData("Progressive Outhouse", ItemClassification.useful), #Type: OUTHOUSE_2
-    ItemData("Progressive Healing Bay", ItemClassification.useful), #Type: HEALING_BAY_2
-    ItemData("Leader Tent", ItemClassification.useful), 
-    ItemData("Progressive Tabernacle", ItemClassification.useful), 
-    ItemData("Progressive Shrine Flame", ItemClassification.useful),
-    ItemData("Progressive Drink House", ItemClassification.useful),
-    ItemData("Re-Indoctrination Stone", ItemClassification.filler), 
-    ItemData("Progressive Hatchery", ItemClassification.useful), 
-    ItemData("Nursery", ItemClassification.useful), 
-    ItemData("Progressive Crypt", ItemClassification.useful), 
-    ItemData("Progressive Morgue", ItemClassification.useful), 
-    ItemData("Progressive Lumberyard", ItemClassification.progression),
-    ItemData("Progressive Stone Mine", ItemClassification.progression), 
-    ItemData("Progressive Missionary", ItemClassification.useful), 
-    ItemData("Progressive Demonic Summoning Circle", ItemClassification.useful), 
-    ItemData("Kitchen", ItemClassification.useful), 
-    ItemData("Fertiliser Storage", ItemClassification.useful), 
-    ItemData("Seed Storage", ItemClassification.useful), 
-    ItemData("Progressive Harvest Totem", ItemClassification.useful), 
+    ItemData("Progressive Shrine & Temple Upgrade", ItemClassification.useful, "Prog_Temple"), #Type: TEMPLE_IV
+    ItemData("Progressive Outhouse", ItemClassification.useful, "Prog_Outhouse"), #Type: OUTHOUSE_2
+    ItemData("Progressive Healing Bay", ItemClassification.useful, "Prog_HealingBay"), #Type: HEALING_BAY_2
+    ItemData("Leader Tent", ItemClassification.useful, "Building_LeaderTent"), 
+    ItemData("Progressive Tabernacle", ItemClassification.useful, "Prog_Tabernacle"), 
+    ItemData("Progressive Shrine Flame", ItemClassification.useful, "Prog_ShrineFlame"),
+    ItemData("Progressive Drink House", ItemClassification.useful, "Prog_Pub"),
+    ItemData("Re-Indoctrination Stone", ItemClassification.filler, "Building_UpgradedIndoctrination"), 
+    ItemData("Progressive Hatchery", ItemClassification.useful, "Prog_Hatchery"), 
+    ItemData("Nursery", ItemClassification.useful, "Building_Daycare"), 
+    ItemData("Progressive Crypt", ItemClassification.useful, "Prog_Crypt"), 
+    ItemData("Progressive Morgue", ItemClassification.useful, "Prog_Morgue"), 
+    ItemData("Progressive Lumberyard", ItemClassification.progression, "Prog_Lumberjack"),
+    ItemData("Progressive Stone Mine", ItemClassification.progression, "Prog_Mine"), 
+    ItemData("Progressive Missionary", ItemClassification.useful, "Prog_Missionary"), 
+    ItemData("Progressive Demonic Summoning Circle", ItemClassification.useful, "Prog_DemonSummoner"), 
+    ItemData("Kitchen", ItemClassification.useful, "Building_Kitchen"), 
+    ItemData("Fertiliser Storage", ItemClassification.useful, "Building_PoopBucket"), 
+    ItemData("Seed Storage", ItemClassification.useful, "Building_SeedBucket"), 
+    ItemData("Progressive Harvest Totem", ItemClassification.useful, "Prog_HarvestTotem"), 
 ]
--- worlds/CotL/locations.py
+++ worlds/CotL/locations.py
@@ -1,4 +1,5 @@
 
+from turtle import position
 import typing
 
 from BaseClasses import Location
@@ -8,9 +9,19 @@ class CotLLocation(Location):  # or from Locations import MyGameLocation
 
 class LocationData:
     locationName : str
+    old_name: str
+    original_item: str
+    menu_name: str
+    tier: int
+    position: str
     region: str
-    def __init__(self, locationName ):
-        self.locationName = locationName 
+    def __init__(self, old_name, original_item, menu_name, tier, position ):
+        self.locationName = f"{menu_name} Tier{tier} {position}"
+        self.old_name = old_name
+        self.menu_name = menu_name
+        self.original_item = original_item
+        self.tier = tier
+        self.position = position
       #  self.region = region
 
 mygame_locations : typing.List[LocationData] = [ 
@@ -20,153 +31,153 @@ mygame_locations : typing.List[LocationData] = [
     #Tier 1 
     #LocationData("Temple"), #Center
     # Building_Temple  
-    LocationData("Sleeping Bags"), #L1
+    LocationData("Sleeping Bags", "Building_Beds", "Shrine", 1, "L1"), #L1
     # Building_Beds  
-    LocationData("Body Pit"), #L2
+    LocationData("Body Pit", "Building_BodyPit", "Shrine", 1, "L2"), #L2
     # Building_BodyPit   
-    LocationData("Farm Plot"), #R1
+    LocationData("Farm Plot", "Building_Farms" "Shrine", 1, "R1"), #R1
     # Building_Farms    
-    LocationData("Farming Bundle 1"), #R2
-    # Building_Farms     
-    LocationData("Farming Bundle 2"), #R2
-    # Building_Farms     
-    LocationData("Tailor"), #R3
+    LocationData("Farming Bundle 1", "Building_FollowerFarming", "Shrine", 1, "R2_1"), #R2
+    # Building_FollowerFarming    
+    LocationData("Farming Bundle 2", "Building_FollowerFarming", "Shrine", 1, "R2_2"), #R2
+    # Building_FollowerFarming     
+    LocationData("Tailor", "Building_Tailor", "Shrine", 1, "R3"), #R3
     # Building_Tailor     
 
     #Tier 2
-    LocationData("Cult II"), #Center
+    LocationData("Cult II", "Building_Temple2", "Shrine", 2, "Center"), #Center
     # Building_Temple2
-    LocationData("Basic Decorations"), #L1
-    # Building_Temple2
-    LocationData("Shelter"), #L2
+    LocationData("Basic Decorations", "Building_Decorations1", "Shrine", 2, "L1"), #L1
+    # Building_Decorations1
+    LocationData("Shelter", "Building_BetterBeds", "Shrine", 2, "L2"), #L2
     # Building_BetterBeds
-    LocationData("Offering Statue"), #L3
+    LocationData("Offering Statue", "Shrine_OfferingStatue", 2, "L3"), #L3
     # Shrine_OfferingStatue
-    LocationData("Tabernacle"), #L4
+    LocationData("Tabernacle", "Shrine_PassiveShrines", "Shrine", 2, "L4"), #L4
     # Shrine_PassiveShrines
-    LocationData("Prison"),#L5
+    LocationData("Prison", "Building_Prison", "Shrine", 2, "L5"),#L5
     #Building_Prison
-    LocationData("Lumberyard"), #R1
+    LocationData("Lumberyard", "Economy_Lumberyard", "Shrine", 2, "R1"), #R1
     # Economy_Lumberyard
-    LocationData("Stone Mine"), #R2
+    LocationData("Stone Mine", "Economy_Mine", "Shrine", 2, "R2"), #R2
     # Economy_Mine
-    LocationData("Missionary"), #R3
+    LocationData("Missionary", "Building_Missionary", "Shrine", 2, "R3"), #R3
     # Building_Missionary
-    LocationData("Demonic Summoning Circle"), #R4
+    LocationData("Demonic Summoning Circle", "Building_DemonSummoner", "Shrine", 2, "R4"), #R4
     # Building_DemonSummoner
-    LocationData("Scarecrow"), #R5
+    LocationData("Scarecrow", "Building_AdvancedFarming", "Shrine", 2, "R5"), #R5
     # Building_AdvancedFarming
 
     #Tier 3
-    LocationData("Refinery"), #Center
+    LocationData("Refinery", "Economy_Refinery", "Shrine", 3, "Center"), #Center
     # Economy_Refinery
-    LocationData("Outhouse"), #L1
+    LocationData("Outhouse", "Building_Outhouse", "Shrine", 3, "L1"), #L1
     # Building_Outhouse
-    LocationData("Healing Bay"), #L2
+    LocationData("Healing Bay", "Building_HealingBay", "Shrine", 3, "L2"), #L2
     # Building_HealingBay
-    LocationData("Janitor Station"), #L3
+    LocationData("Janitor Station", "Building_JanitorStation", "Shrine", 3, "L3"), #L3
     # Building_JanitorStation
-    LocationData("Empowered Shrine of Disciples"), #L4
+    LocationData("Empowered Shrine of Disciples", "Building_Shrine_Disciple_Boost", "Shrine", 3, "L4"), #L4
     # Building_Shrine_Disciple_Boost
-    LocationData("Cheaper Rituals"), #L5
+    LocationData("Cheaper Rituals", "Temple_CheaperRituals", "Shrine", 3, "L5"), #L5
     # Temple_CheaperRituals
-    LocationData("Shrine Flame Bundle"),#L6
+    LocationData("Shrine Flame Bundle", "Shrine_Flame", "Shrine", 3, "L6"),#L6
     # Shrine_Flame
-    LocationData("Confession Booth"), #L7
+    LocationData("Confession Booth", "Building_ConfessionBooth", "Shrine", 3, "L7"), #L7
     # Building_ConfessionBooth
-    LocationData("Propaganda Speakers"), #L8
+    LocationData("Propaganda Speakers", "Building_PropagandaSpeakers", "Shrine", 3, "L8"), #L8
     # Building_PropagandaSpeakers
-    LocationData("Crypt I"), #L9
+    LocationData("Crypt I", "Building_Crypt_1", "Shrine", 3, "L9"), #L9
     # Building_Crypt_1
-    LocationData("Missionary II"), #R1
+    LocationData("Missionary II", "Building_MissionaryII", "Shrine", 3, "R1"), #R1
     # Building_MissionaryII
-    LocationData("Demonic Summoning Circle II"), #R2
+    LocationData("Demonic Summoning Circle II", "Building_DemonSummoner_2", "Shrine", 3, "R2"), #R2
     # Building_DemonSummoner_2
-    LocationData("Fertiliser Silo"), #R3
+    LocationData("Fertiliser Silo", "Building_SiloFertiliser", "Shrine", 3, "R3"), #R3
     # Building_SiloFertiliser
-    LocationData("Harvest Totem"), #R4
+    LocationData("Harvest Totem", "Building_HarvestTotem", "Shrine", 3, "R4"), #R4
     # Building_HarvestTotem
 
     #Tier 4
-    LocationData("Cult III"), #Center
+    LocationData("Cult III", "Temple_III", "Shrine", 4, "Center"), #Center
     # Temple_III
-    LocationData("Shared Shelter"), #L1
+    LocationData("Shared Shelter", "Building_Shared_House", "Shrine", 4, "L1"), #L1
     # Building_Shared_House
-    LocationData("Grand Shelter"), #L2
+    LocationData("Grand Shelter", "Building_Beds3", "Shrine", 4, "L2"), #L2
     # Building_Beds3
-    LocationData("Janitor Station II"), #L3
+    LocationData("Janitor Station II", "Building_JanitorStation_2", "Shrine", 4, "L3"), #L3
     # Building_JanitorStation_2
-    LocationData("Collected Shrine of Disciples"), #L4
+    LocationData("Collected Shrine of Disciples", "Building_Shrine_Disciple_Collection", "Shrine", 4, "L4"), #L4
     # Building_Shrine_Disciple_Collection
-    LocationData("Ritual Cool Downs"), #L5
+    LocationData("Ritual Cool Downs", "Temple_FasterCoolDowns", "Shrine", 4, "L5"), #L5
     # Temple_FasterCoolDowns
-    LocationData("Tabernacle II"), #L6
+    LocationData("Tabernacle II", "Shrine_PassiveShrinesII", "Shrine", 4, "L6"), #L6
     # Shrine_PassiveShrinesII
-    LocationData("Shrine Flame II"), #L7
+    LocationData("Shrine Flame II", "Shrine_FlameII", "Shrine", 4, "L7"), #L7
     # Shrine_FlameII
-    LocationData("Drink House"), #L8
+    LocationData("Drink House", "Building_Pub", "Shrine", 4, "L8"), #L8
     # Building_Pub
-    LocationData("Drum Circle"), #L9
+    LocationData("Drum Circle", "Building_Drum", "Shrine", 4, "L9"), #L9
     # Building_Drum
-    LocationData("Mating Tent 1"), #L10
+    LocationData("Mating Tent 1", "Building_MatingTent", "Shrine", 4, "L10_1"), #L10
     # Building_MatingTent
-    LocationData("Mating Tent 2"), #L10
+    LocationData("Mating Tent 2", "Building_MatingTent", "Shrine", 4, "L10_2"), #L10
     # Building_MatingTent
-    LocationData("Crypt II"), #L11
+    LocationData("Crypt II", "Building_Crypt_2", "Shrine", 4, "L11"), #L11
     # Building_Crypt_2
-    LocationData("Morgue I"), #L12
+    LocationData("Morgue I", "Building_Morgue_1", "Shrine", 4, "L12"), #L12
     # Building_Morgue_1
-    LocationData("Refinery II"), #R1
+    LocationData("Refinery II", "Economy_Refinery_2", "Shrine", 4, "R1"), #R1
     # Economy_Refinery_2
-    LocationData("Bone Decorations"), #R2
+    LocationData("Bone Decorations", "Building_Decorations2", "Shrine", 4, "R2"), #R2
     # Building_Decorations2
-    LocationData("Farm Station II"), #R3
+    LocationData("Farm Station II", "Building_FarmStationII", "Shrine", 4, "R3"), #R3
     # Building_FarmStationII
-    LocationData("Compost"), #R4
+    LocationData("Compost", "Followers_Compost", "Shrine", 4, "R4"), #R4
     # Followers_Compost
-    LocationData("Trap Scarecrow"), #R5
+    LocationData("Trap Scarecrow", "Building_Scarecrow2", "Shrine", 4, "R5"), #R5
     # Building_Scarecrow2
 
     #Tier 5
-    LocationData("Cult IV"), #Center
+    LocationData("Cult IV", "Temple_IV", "Shrine", 5, "Center"), #Center
     # Temple_IV
-    LocationData("Outhouse II"), #L1 
+    LocationData("Outhouse II", "Building_Outhouse2", "Shrine", 5, "L1"), #L1 
     # Building_Outhouse2
-    LocationData("Healing Bay II"), #L2 
+    LocationData("Healing Bay II", "Building_HealingBay2", "Shrine", 5, "L2"), #L2 
     # Building_HealingBay2
-    LocationData("Leader Tent"), #L3 
+    LocationData("Leader Tent", "Building_LeaderTent", "Shrine", 5, "L3"), #L3 
     # Building_LeaderTent
-    LocationData("Tabernacle III"), #L4 
+    LocationData("Tabernacle III", "Shrine_PassiveShrinesIII", "Shrine", 5, "L4"), #L4 
     # Shrine_PassiveShrinesIII
-    LocationData("Shrine Flame III"), #L5 
+    LocationData("Shrine Flame III", "Shrine_FlameIII", "Shrine", 5, "L5"), #L5 
     # Shrine_FlameIII
-    LocationData("Drinkhouse II"), #L6 
+    LocationData("Drinkhouse II", "Building_Pub_2", "Shrine", 5, "L6"), #L6 
     # Building_Pub_2
-    LocationData("Re-Indoctrination Stone"), #L7 
+    LocationData("Re-Indoctrination Stone", "Building_UpgradedIndoctrination", "Shrine", 5, "L7"), #L7 
     # Building_UpgradedIndoctrination
-    LocationData("Hatchery II"), #L8 
+    LocationData("Hatchery II", "Building_Hatchery_2", "Shrine", 5, "L8"), #L8 
     # Building_Hatchery_2
-    LocationData("Nursery"), #L9 
+    LocationData("Nursery", "Building_Daycare", "Shrine", 5, "L9"), #L9 
     # Building_Daycare
-    LocationData("Crypt III"), #L10 
+    LocationData("Crypt III", "Building_Crypt_3", "Shrine", 5, "L10"), #L10 
     # Building_Crypt_3
-    LocationData("Morgue II"), #L11 
+    LocationData("Morgue II", "Building_Morgue_2", "Shrine", 5, "L11"), #L11 
     # Building_Morgue_2
-    LocationData("Lumberyard II"), #R1 
+    LocationData("Lumberyard II", "Economy_LumberyardII", "Shrine", 5, "R1"), #R1 
     # Economy_LumberyardII
-    LocationData("Stone Mine II"), #R2
+    LocationData("Stone Mine II", "Economy_MineII", "Shrine", 5, "R2"), #R2
     # Economy_MineII
-    LocationData("Missionary III"), #R3
+    LocationData("Missionary III", "Building_MissionaryIII", "Shrine", 5, "R3"), #R3
     # Building_MissionaryIII
-    LocationData("Demonic Summoning Circle III"), #R4
+    LocationData("Demonic Summoning Circle III", "Building_DemonSummoner_3", "Shrine", 5, "R4"), #R4
     # Building_DemonSummoner_3
-    LocationData("Kitchen"), #R5
+    LocationData("Kitchen", "Building_Kitchen", "Shrine", 5, "R5"), #R5
     # Building_Kitchen
-    LocationData("Fertiliser Storage"), #R6
+    LocationData("Fertiliser Storage", "Building_PoopBucket", "Shrine", 5, "R6"), #R6
     # Building_PoopBucket
-    LocationData("Seed Storage"), #R7
+    LocationData("Seed Storage", "Building_SeedBucket", "Shrine", 5, "R7"), #R7
     # Building_SeedBucket
-    LocationData("Devotion Harvest Totem"), #R8
+    LocationData("Devotion Harvest Totem", "Building_HarvestTotem2", "Shrine", 5, "R8"), #R8
     # Building_HarvestTotem2
 
 
